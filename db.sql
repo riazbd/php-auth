fbe1566d073963ff25452912c5ee724815e298cf
Hoppe It <email@emai.com>
2022-06-13 18:31:58 +0600

    Add Email field in db.sql

--- db.sql
+++ db.sql
@@ -31,10 +31,11 @@ USE `lr`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
   `password` varchar(64) NOT NULL,
   `salt` varchar(32) NOT NULL,
   `name` varchar(50) NOT NULL,
-  `joined` datetime NOT NULL,
+  `join_date` datetime NOT NULL,
   `group` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=16 ;
